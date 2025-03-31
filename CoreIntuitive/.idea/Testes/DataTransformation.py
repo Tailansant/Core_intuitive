fe8133eb40e0b4233745aa619e507fd7092254a5
Tailan <tailan.santosts7@gmail.com>
2025-03-31 18:07:43 -0300

    realizando alterações na classe WebScraping

--- CoreIntuitive/.idea/Testes/DataTransformation.py
+++ CoreIntuitive/.idea/Testes/DataTransformation.py
@@ -1,15 +1,16 @@
+# Agora, passando o diretório para o código de transformação de dados
 import pdfplumber
 import pandas as pd
 import zipfile
+
+PDF_PATH = os.path.join(save_directory, 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')  # Defina o caminho completo do PDF
 
 class DataTransformation:
     def __init__(self, pdf_path, csv_output_path):
         self.pdf_path = pdf_path
         self.csv_output_path = csv_output_path
         
-    
     def extract_file_data(self):
-        
         datas = []
         
         with pdfplumber.open(self.pdf_path) as pdf_file:
@@ -19,30 +20,32 @@ class DataTransformation:
                     if line.strip():
                         datas.append(line.split())
                         
-                        
         return datas
     
-    
     def save_csv(self, datas):
-                
         data_frame = pd.DataFrame(datas, columns=['Procedimento', 'Categoria'])
         data_frame.to_csv(self.csv_output_path, index=False)
-        
         print(f"Data saved in {self.csv_output_path}")
         
-        
     def substitute_abbreviation(self):
-        
         substitute =  {'OD': 'Descrição Completa OD', 'AMB': 'Descrição Completa AMB'}
         
         data_frame = pd.read_csv(self.csv_output_path)
         data_frame.replace(substitute, inplace=True)
         data_frame.to_csv(self.csv_output_path, index=False)
         
-        
     def compact_csv(self):
-        
-        with zipfile.ZiptFile('teste_tailan.zip', 'w') as zip_file:
+        with zipfile.ZipFile('teste_tailan.zip', 'w') as zip_file:
             zip_file.write(self.csv_output_path)
-            
-        print(f"file compacted")
+        print(f"File compacted")
+
+
+def main_transformation():
+    transformer = DataTransformation(pdf_path=PDF_PATH, csv_output_path='datas.csv')
+    data_extract = transformer.extract_file_data()
+    transformer.save_csv(data_extract)
+    transformer.substitute_abbreviation()
+    transformer.compact_csv()
+
+# Chama a função de transformação
+main_transformation()
--- CoreIntuitive/.idea/Testes/WebScraping.py
+++ CoreIntuitive/.idea/Testes/WebScraping.py
@@ -9,20 +9,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class WebScraping:
-    def __init__(self, url):
+    def __init__(self, url, save_directory):
         self.url = url
+        self.save_directory = save_directory  
         self.driver = self.init_driver()
 
     def init_driver(self):
+        
         try:
             chrome_options = Options()
             chrome_options.headless = True
             driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            
             return driver
         except Exception as e:
             raise ValueError(f"Error with the WebDriver: {e}")
 
     def acessing_site(self):
+        
         try:
             self.driver.get(self.url)
             self.driver.implicitly_wait(10)
@@ -30,13 +34,21 @@ class WebScraping:
             raise ValueError(f"Error accessing the site: {e}")
 
     def downloading_files(self, urls):
+        
         try:
+
+            if not os.path.exists(self.save_directory):
+                os.makedirs(self.save_directory)
+
             for url in urls:
+                file_name = url.split('/')[-1]
+                file_path = os.path.join(self.save_directory, file_name)  
+                
                 response = requests.get(url)
                 if response.status_code == 200:
-                    with open(url.split('/')[-1], 'wb') as file:
+                    with open(file_path, 'wb') as file:
                         file.write(response.content)
-                    print(f"File downloaded: {url.split('/')[-1]}")
+                    print(f"File downloaded: {file_name}")
                 else:
                     print(f"Failed to download {url}. HTTP Status: {response.status_code}")
 
@@ -44,15 +56,24 @@ class WebScraping:
         except Exception as e:
             raise ValueError(f"Error downloading files: {e}")
 
+
     def compressing_files(self):
+        
         try:
-            with zipfile.ZipFile('anexos.zip', 'w') as zip_file:
-                for file in os.listdir('.'):
+            zip_path = os.path.join(self.save_directory, 'anexos.zip') 
+            
+            with zipfile.ZipFile(zip_path, 'w') as zip_file:
+                for file in os.listdir(self.save_directory):
                     if file.endswith('.pdf'):
-                        zip_file.write(file)
+                        zip_file.write(os.path.join(self.save_directory, file), file)  
+                        
             print("Files compressed!")
+            
+            print(f"O script está sendo executado no diretório: {self.save_directory}")
+            
         except Exception as e:
             raise ValueError(f"Error compressing files: {e}")
+        
 
     def closing_table(self):
         try:
@@ -60,10 +81,12 @@ class WebScraping:
             self.driver.quit()
         except Exception as e:
             raise ValueError(f"Error closing the browser: {e}")
+
         
 def main():
+    save_directory = './downloaded_files'  
     url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
-    web_scraper = WebScraping(url)
+    web_scraper = WebScraping(url, save_directory)
     web_scraper.acessing_site()
 
     files_urls = [
@@ -75,5 +98,8 @@ def main():
     web_scraper.compressing_files()
     web_scraper.closing_table()
 
+    return save_directory  
 
-main()
+save_directory = main()
+
+
